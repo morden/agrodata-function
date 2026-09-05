0b9ea2345afd849798d3bca3e24ebaad4ba507e0
morden <mauricioorden@gmail.com>
2026-09-05 16:40:52 -0300

    Update index.js

--- agrodata/index.js
+++ agrodata/index.js
@@ -108,7 +108,7 @@ module.exports = async function (context, req) {
   <footer>
     Region: ${REGION}<br>
     Desplegado desde GitHub<br>
-    Ninguna VM fue lanzada para servir esta pagina.
+    Ninguna Maquina virtual fue lanzada para servir esta pagina.
   </footer>
 </main>
 </body>
